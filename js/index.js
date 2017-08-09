87ca21ade3c4f7bbacb4ea96a0bf53da12a0e075
tongjin <aotian16@gmail.com>
2017-08-09 16:13:46 +0800

    modify index.js

--- js/index.js
+++ js/index.js
@@ -4,11 +4,11 @@ $(document).ready(
         // 链接: 鼠标经过显示下划线
         $("a").on({
             "mouseover": function () {
-                $("a").css("text-decoration", "underline");
+                $(this).css("text-decoration", "underline");
                 // console.log("over");
             },
             "mouseout": function () {
-                $("a").css("text-decoration", "none");
+                $(this).css("text-decoration", "none");
                 // console.log("out");
             }
         });
